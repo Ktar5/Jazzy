63ca28b3c74795f35862020c876c8e90f3c99cc6
Carter Gale <buildfresh@gmail.com>
2018-03-14 15:22:49 -0700

    Fixed editor view panning

--- src/main/java/com/ktar5/mapeditor/javafx/centerview/editor/EditorPane.java
+++ src/main/java/com/ktar5/mapeditor/javafx/centerview/editor/EditorPane.java
@@ -3,6 +3,7 @@ package com.ktar5.mapeditor.javafx.centerview.editor;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import lombok.Getter;
+import org.pmw.tinylog.Logger;
 
 @Getter
 public class EditorPane extends ScrollPane {
@@ -20,18 +21,21 @@ public class EditorPane extends ScrollPane {
         this.setContent(canvas);
 
         //Canvas drag
-        addEventFilter(MouseEvent.MOUSE_CLICKED, event -> {
+        this.addEventFilter(MouseEvent.MOUSE_RELEASED, e -> {
+            Logger.debug("End dragging");
             canvas.isDragging = false;
         });
 
         addEventFilter(MouseEvent.MOUSE_DRAGGED, event -> {
             if (!canvas.isDragging) {
+                Logger.debug("Set canvas initial drag values");
                 canvas.pressedX = event.getSceneX();
                 canvas.pressedY = event.getSceneY();
                 canvas.origX = canvas.getTranslateX();
                 canvas.origY = canvas.getTranslateY();
                 canvas.isDragging = true;
             }
+            //Logger.debug("Perform drag on canvas");
             canvas.setTranslateX(canvas.origX + (event.getSceneX() - canvas.pressedX));
             canvas.setTranslateY(canvas.origY + (event.getSceneY() - canvas.pressedY));
 
