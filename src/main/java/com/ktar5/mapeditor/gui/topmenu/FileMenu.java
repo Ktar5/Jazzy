618aff6aef4793721b283dd4605624e8ed7a7769
Carter Gale <buildfresh@gmail.com>
2018-03-21 15:33:58 -0700

    Created menu accelerator (keyboard shortcuts)

--- src/main/java/com/ktar5/mapeditor/gui/topmenu/FileMenu.java
+++ src/main/java/com/ktar5/mapeditor/gui/topmenu/FileMenu.java
@@ -7,6 +7,7 @@ import com.ktar5.mapeditor.tileset.BaseTileset;
 import com.ktar5.mapeditor.tileset.TilesetManager;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
+import javafx.scene.input.KeyCombination;
 
 public class FileMenu extends Menu {
 
@@ -19,6 +20,7 @@ public class FileMenu extends Menu {
                 Main.root.getCenterView().getEditorViewPane().setSelectedTab(baseTilemap.getId());
             }
         });
+        openTilemap.setAccelerator(KeyCombination.keyCombination("SHORTCUT+O"));
 
         final MenuItem openTileset = new MenuItem("Open Tileset");
         openTileset.setOnAction(event -> {
@@ -27,18 +29,29 @@ public class FileMenu extends Menu {
                 Main.root.getCenterView().getEditorViewPane().setSelectedTab(baseTileset.getId());
             }
         });
+        openTileset.setAccelerator(KeyCombination.keyCombination("SHORTCUT+SHIFT+O"));
 
         final MenuItem newMap = new MenuItem("New Tilemap");
         newMap.setOnAction(event -> MapManager.get().createMap());
+        newMap.setAccelerator(KeyCombination.keyCombination("SHORTCUT+N"));
 
         final MenuItem newTileset = new MenuItem("New Tileset");
         newTileset.setOnAction(event -> TilesetManager.get().createTileset());
+        newTileset.setAccelerator(KeyCombination.keyCombination("SHORTCUT+SHIFT+N"));
 
         final MenuItem save = new MenuItem("Save Current");
-        save.setOnAction(event -> Main.root.getCurrentTab().getTabbable().save());
+        save.setOnAction(event -> {
+            if (Main.root.getCurrentTab() != null)
+                Main.root.getCurrentTab().getTabbable().save();
+        });
+        save.setAccelerator(KeyCombination.keyCombination("SHORTCUT+S"));
 
         final MenuItem saveAs = new MenuItem("Save As..");
-        saveAs.setOnAction(event -> Main.root.getCurrentTab().getTabbable().saveAs());
+        saveAs.setOnAction(event -> {
+            if (Main.root.getCurrentTab() != null)
+                Main.root.getCurrentTab().getTabbable().saveAs();
+        });
+        saveAs.setAccelerator(KeyCombination.keyCombination("SHORTCUT+SHIFT+S"));
 
         this.getItems().addAll(
                 newMap,
